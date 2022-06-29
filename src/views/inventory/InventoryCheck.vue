910d23f25ba3003e36186705175cfff2565a2048
SUNLIFAN <1656680420@qq.com>
2022-06-29 19:26:34 +0800

    fix bug: duplicate rows when exporting excel

--- src/views/inventory/InventoryCheck.vue
+++ src/views/inventory/InventoryCheck.vue
@@ -2,7 +2,7 @@
   <Layout>
     <Title title="库存盘点"></Title>
     （盘点的是当天的库存快照，包括当天的各种商品的名称，型号，库存数量，库存均价，批次，批号，出厂日期，并且显示行号。要求可以导出Excel。
-    <el-button @click="exportAsExcel">导出Excel</el-button>
+    <el-button @click="exportAsExcel('out-table', 'inventory' + Date.now() + '.xlsx')">导出Excel</el-button>
     <div class="table-body">
       <el-table
         :data="cur_list"
@@ -120,24 +120,34 @@ export default {
 
   },
   methods: {
-    exportAsExcel() {
+    exportAsExcel(id, title) {
       //TODO 导出Excel
-      let tables = document.getElementById("out-table");
-                let table_book = this.$XLSX.utils.table_to_book(tables);
-                var table_write = this.$XLSX.write(table_book, {
-                    bookType: "xlsx",
-                    bookSST: true,
-                    type: "array"
-                });
-                try {
-                    this.$FileSaver.saveAs(
-                        new Blob([table_write], { type: "application/octet-stream" }),
-                        "sheetjs.xlsx"
-                    );
-                } catch (e) {
-                    if (typeof console !== "undefined") console.log(e, table_write);
-                }
-                return table_write;
+      /* generate workbook object from table */
+    var fix = document.querySelector('.el-table__fixed');
+    var wb;
+    if (fix) {
+      wb = this.$XLSX.utils.table_to_book(document.getElementById(id).removeChild(fix));
+      document.getElementById(id).appendChild(fix);
+    } else {
+      wb = this.$XLSX.utils.table_to_book(document.getElementById(id));
+    }
+    /* get binary string as output */
+    var wbout = this.$XLSX.write(wb, {
+      bookType: 'xlsx',
+      bookSST: true,
+      type: 'array'
+    });
+    try {
+      this.$FileSaver.saveAs(
+        new Blob([wbout], {
+          type: 'application/octet-stream'
+        }),
+        title
+      );
+    } catch (e) {
+      if (typeof console !== 'undefined') console.log(e, wbout);
+    }
+    return wbout;
     },
     showProduct(row) {
       this.cur_row = row;
