d7b83e0346ce46ea993a7adea2b2a0393d3870cb
ZH Cai <201250127@smail.nju.edu.cn>
2022-07-06 10:55:23 +0800

    正在调试 经营历程表 页面

--- src/views/financialManagement/OperateSheetView.vue
+++ src/views/financialManagement/OperateSheetView.vue
@@ -1,13 +1,94 @@
 <template>
-
+  <Layout>
+    <Title title="查看经营情况表"></Title>
+    <div>
+      <span><strong>请选择一个时间段: </strong></span>
+      <el-date-picker
+          v-model="date"
+          type="datetimerange"
+          range-separator="至"
+          start-placeholder="开始日期"
+          end-placeholder="结束日期"
+          @change="getOperateSheet()">
+      </el-date-picker>
+      <div v-if="date !== ''" class="mt15">
+        <h4>{{beginDate}}&nbsp;至&nbsp;{{endDate}} 内的</h4>
+        <div class="mt15">
+          <span><strong> 折让后总收入合计为: </strong></span>
+          <span>{{finalIncome}}</span>
+        </div>
+        <div class="mt15">
+          <span><strong>折让总额合计为: </strong></span>
+          <span>{{discountAmount}}</span>
+        </div>
+        <div class="mt15">
+          <span><strong>总支出合计为: </strong></span>
+          <span>{{cost}}</span>
+        </div>
+        <div class="mt15">
+          <span><strong>利润合计为: </strong></span>
+          <span>{{profit}}</span>
+        </div>
+      </div>
+    </div>
+  </Layout>
 </template>
 <!-- TODO -->
 <script>
+import Layout from "@/components/content/Layout";
+import Title from "@/components/content/Title";
+import { formatDate } from "@/common/utils";
+import { showOperateSheet } from "@/network/financialManagement";
+
 export default {
-  name: "OperateSheetView"
+  name: "OperateSheetView",
+  components: {
+    Layout,
+    Title
+  },
+  data() {
+    return {
+      date: '',
+      finalIncome: '',      // 折让后总额
+      discountAmount: '',   // 折让金额
+      cost: '',             // 总支出
+      profit: ''            // 利润
+    }
+  },
+  computed: {
+    beginDate: function () {
+      return this.date === '' ? '' : formatDate(this.date[0])
+    },
+    endDate: function () {
+      return this.date === '' ? '' : formatDate(this.date[1])
+    }
+  },
+  methods: {
+    getOperateSheet() {
+      const config = {
+        params: {
+          begin: this.beginDate,
+          end: this.endDate
+        }
+      }
+      showOperateSheet(config).then(_res => {
+        let res = _res.result
+        this.finalIncome = res.finalIncome
+        this.discountAmount = res.discountAmount
+        this.cost = res.cost
+        this.profit = res.profit
+      })
+    },
+    filterTag(value, row) {
+      return row.type() === value
+    },
+    formatDate
+  }
 }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+.mt15 {
+  margin-top: 15px;
+}
 </style>
