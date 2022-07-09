c8d3d3ce6caab518fba997e171cdd2eee49705fb
ZH Cai <201250127@smail.nju.edu.cn>
2022-07-09 10:55:08 +0800

    正在调试 基于总价的促销策略制定

--- src/views/promotion/TotalPricePromotionManagement.vue
+++ src/views/promotion/TotalPricePromotionManagement.vue
@@ -1,104 +1,198 @@
 <template>
   <Layout>
-    <Title title="员工管理"></Title>
-    <el-button type="primary" size="medium" @click="dialogVisible = true">新增员工</el-button>
-    <div style="margin-top: 10px">
-      <el-table
-          :data="staffList"
-          stripe
-          style="width: 100%"
-          :header-cell-style="{'text-align':'center'}"
-          :cell-style="{'text-align':'center'}">
-        <el-table-column
-            prop="id"
-            label="id"
-            width="60">
-        </el-table-column>
-        <el-table-column
-            prop="name"
-            label="姓名"
-            width="100">
-        </el-table-column>
-        <el-table-column
-            prop="gender"
-            label="性别"
-            width="100">
-        </el-table-column>
-        <el-table-column
-            prop="birthday"
-            label="出生日期"
-            width="100">
-        </el-table-column>
-        <el-table-column
-            prop="phone"
-            label="手机号"
-            width="150">
-        </el-table-column>
-        <el-table-column
-            prop="position"
-            label="职位"
-            width="150">
-        </el-table-column>
-        <el-table-column
-            prop="balance"
-            label="工资卡账户"
-            width="100">
-        </el-table-column>
-        <el-table-column
-            label="操作">
-          <template slot-scope="scope">
-            <el-button
-                @click="showEditDialog(scope.row.id)"
-                type="text"
-                size="small">
-              编辑
-            </el-button>
-          </template>
-        </el-table-column>
-      </el-table>
+    <Title title="基于总价的促销策略"></Title>
+    <el-button type="primary" size="medium" @click="dialogVisible = true">制定促销策略</el-button>
+    <div class="body">
+      <total-price-promotion-list :list="promotionList" @refresh="getData()"/>
     </div>
-    <el-dialog title="新增员工"
-               :visible.sync="dialogVisible"
-               width="40%"
-               :before-close="handleClose">
+    <el-dialog
+        title="创建销售策略"
+        :visible.sync="dialogVisible"
+        width="40%"
+        :before-close="handleClose">
       <div style="width: 90%; margin: 0 auto">
-        <el-form :model="staffForm" label-width="100px" ref="staffForm">
-          <el-form-item label="姓名: " prop="name">
-            <el-input v-model="staffForm.name"></el-input>
+        <el-form :model="promotionForm" label-width="100px" ref="promotionForm" :rules="rules">
+          <el-form-item label="时间段" prop="date">
+            <el-date-picker
+                v-model="promotionForm.date"
+                type="datetimerange"
+                range-separator="至"
+                start-placeholder="开始日期"
+                end-placeholder="结束日期">
+            </el-date-picker>
           </el-form-item>
-          <el-form-item label="性别: " prop="gender">
-            <el-select v-model="staffForm.gender" placeholder="请选择性别">
-              <el-option
-                  v-for="item in genderList" :key="item.id" :value="item">
-                {{ item }}
-              </el-option>
-            </el-select>
+          <el-form-item label="总价阈值: " prop="threshold">
+            <el-input v-model="promotionForm.threshold"></el-input>
           </el-form-item>
-          <el-form-item label="出生日期: " prop="birthday">
-            <el-input v-model="staffForm.birthday"></el-input>
+          <el-form-item label="优惠券金额: " prop="voucherAmount">
+            <el-input v-model="promotionForm.voucherAmount"></el-input>
           </el-form-item>
-          <el-form-item label="手机号: " prop="phone">
-            <el-input v-model="staffForm.phone"></el-input>
+          <el-form-item
+              v-for="(item, index) in promotionForm.contentList"
+              :key="index"
+              :label="'赠品' + (index + 1)">
+            <div>
+              <el-select v-model="item.pid" placeholder="请选择商品id" style="width: 40%; margin-right: 5%">
+                <el-option
+                    v-for="item1 in commodityList"
+                    :key="item1.id"
+                    :label="item1.id"
+                    :value="item1.id">
+                </el-option>
+              </el-select>
+              <el-input v-model="item.quantity" style="width: 25%; margin-right: 5%" placeholder="请输入商品数量"></el-input>
+              <el-input v-model="item.unitPrice" style="width: 25%;" placeholder="请输入商品单价"></el-input>
+            </div>
+            <div style="margin-top: 10px">
+              <el-input v-model="item.remark" style="width: 70%; margin-right: 10%" placeholder="请填写备注"></el-input>
+              <el-button type="text" size="small" @click="addProduct" v-if="index === promotionForm.contentList.length - 1">添加</el-button>
+              <el-button type="text" size="small" @click.prevent="removeProduct(item)" v-if="index !== 0">删除</el-button>
+            </div>
           </el-form-item>
-          <el-form-item label="职位: " prop="position">
-            <el-input v-model="staffForm.position"></el-input>
+          <el-form-item label="备注: ">
+            <el-input type="textarea" v-model="promotionForm.remark"></el-input>
           </el-form-item>
         </el-form>
       </div>
       <span slot="footer" class="dialog-footer">
-        <el-button type="primary" @click="submitForm('staffForm')">确认新增</el-button>
+        <el-button type="primary" @click="submitForm('promotionForm')">立即创建</el-button>
       </span>
     </el-dialog>
-
   </Layout>
 </template>
 
 <script>
+import Layout from "@/components/content/Layout";
+import Title from "@/components/content/Title";
+import TotalPricePromotionList from "@/views/promotion/components/TotalPricePromotionList";
+import {getAllTotalPricePromotion, createTotalPricePromotion} from "@/network/promotion";
+import {getAllCommodity} from "@/network/commodity";
+import {formatDate} from "@/common/utils";
 export default {
-  name: "TotalPricePromotionManagement"
+  name: "TotalPricePromotionManagement",
+  components: {
+    Layout,
+    Title,
+    TotalPricePromotionList
+  },
+  computed: {
+    beginDate: function () {
+      return this.promotionForm.date === '' ? '' : formatDate(this.promotionForm.date[0])
+    },
+    endDate: function () {
+      return this.promotionForm.date === '' ? '' : formatDate(this.promotionForm.date[1])
+    }
+  },
+  data() {
+    return {
+      commodityList: [],
+      dialogVisible: false,
+      promotionList: [],
+      promotionForm: {
+        contentList: [
+          {
+            pid: '',
+            quantity: '',
+            unitPrice: '',
+            remark: ''
+          }
+        ]
+      },
+      rules: {
+        date: [
+          { required: true, message: '请选择时间', trigger: 'change' }
+        ],
+        threshold: [
+          { required: true, message: '请输入总价阈值', trigger: 'change' }
+        ],
+        voucherAmount: [
+          { required: true, message: '请输入优惠券金额', trigger: 'change' }
+        ],
+      }
+    }
+  },
+  mounted() {
+    this.getData()
+    getAllCommodity({}).then(_res => {
+      let res = _res.result
+      res.forEach(item => this.commodityList.push(item))
+    })
+  },
+  methods: {
+    getData() {
+      getAllTotalPricePromotion({}).then(_res => {
+        this.promotionList = _res.result
+      })
+    },
+    handleClose(done) {
+      this.$confirm('确认关闭？')
+          .then(_ => {
+            this.resetForm()
+            done();
+          })
+          .catch(_ => {});
+    },
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          this.promotionForm.id = null
+          this.promotionForm.thredhold = Number(this.promotionForm.threshold)
+          this.promotionForm.voucherAmount = Number(this.promotionForm.voucherAmount)
+          this.promotionForm.beginTimeStr = this.beginDate
+          this.promotionForm.endTimeStr = this.endDate
+          if (this.promotionForm.hasContent === false) {
+            this.promotionForm.contentList = []
+          } else {
+            this.promotionForm.contentList.forEach((item) => {
+              item.id = null
+              item.quantity = parseInt(item.quantity)
+              item.unitPrice = parseInt(item.unitPrice)
+              item.totalAmount = item.quantity * item.unitPrice
+            })
+          }
+          createTotalPricePromotion(this.promotionForm).then(_res => {
+            if (_res.msg === 'Success') {
+              this.$message.success('创建成功!')
+              this.dialogVisible = false
+              this.resetForm()
+              this.getData()
+            }
+          })
+        }
+      })
+    },
+    resetForm() {
+      this.promotionForm = {
+        contentList: [
+          {
+            pid: '',
+            quantity: '',
+            unitPrice: '',
+            remark: ''
+          }
+        ]
+      }
+    },
+    addProduct() {
+      this.promotionForm.contentList.push({});
+    },
+    removeProduct(item) {
+      let index = this.promotionForm.contentList.indexOf(item)
+      if (index !== -1) {
+        this.promotionForm.contentList.splice(index, 1)
+      }
+    }
+  }
 }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+.body {
+  margin: 0 auto;
+  margin-top: 10px;
+  height: 80vh;
+  overflow-y: auto;
+  width: 100%;
+  background: rgba($color: #fff, $alpha: 0.5);
+}
 </style>
--- src/views/promotion/components/TotalPricePromotionList.vue
+++ src/views/promotion/components/TotalPricePromotionList.vue
@@ -1,13 +1,153 @@
 <template>
-
+  <div class="card">
+    <el-card v-for="(item, index) in list" :index="item.index" :key="item.id" shadow="hover">
+      <template #header>
+        <el-row>
+          <el-col :span="18">
+            <span><strong>id: </strong>{{item.id}}</span>
+            <el-popconfirm
+                title="确定要删除吗？"
+                @confirm="deletePromotion(item.id)"
+            >
+              <el-button style="margin-left: 10px"
+                         type="danger" size="mini" slot="reference">
+                删除
+              </el-button>
+            </el-popconfirm>
+         </el-col>
+          <el-col :span="6">
+            <el-button class="button" type="text"
+                       v-if="!showAll[index]"
+                       @click="changeState(index)">
+              展开
+            </el-button>
+            <el-button class="button" type="text"
+                       v-if="showAll[index]"
+                       @click="changeState(index)">
+              收起</el-button>
+          </el-col>
+        </el-row>
+      </template>
+      <div>
+        <el-row>
+          <el-col :span="8">
+            <span><strong>开始时间: </strong>{{formatDate(item.beginTimeStr)}}</span>
+          </el-col>
+          <el-col :span="8">
+            <span><strong>结束时间: </strong>{{formatDate(item.endTimeStr)}}</span>
+          </el-col>
+          <el-col :span="6">
+            <span><strong>生效阈值: </strong>{{item.threshold}}(元)</span>
+          </el-col>
+        </el-row>
+        <el-row style="margin-top: 15px">
+          <el-col :span="3">
+            <span><strong>操作员 </strong>{{item.operator}}</span>
+          </el-col>
+          <el-col :span="6">
+            <span><strong>优惠券金额 </strong>{{item.voucherAmount}}(元)</span>
+          </el-col>
+        </el-row>
+        <el-row style="margin-top: 15px">
+          <el-col :span="24">
+            <span><strong>备注: </strong>{{item.remark}}</span>
+          </el-col>
+        </el-row>
+        <div v-if="showAll[index]" style="margin-top: 15px">
+          <div style="margin-bottom: 15px"><strong>赠品清单:</strong></div>
+          <el-table
+              :data="item.contentList"
+              stripe
+              style="width: 100%"
+              :header-cell-style="{'text-align':'center'}"
+              :cell-style="{'text-align':'center'}">
+            <el-table-column
+                prop="id"
+                label="id"
+                width="180">
+            </el-table-column>
+            <el-table-column
+                prop="pid"
+                label="商品id"
+                width="180">
+            </el-table-column>
+            <el-table-column
+                prop="quantity"
+                label="数量"
+                width="200">
+            </el-table-column>
+            <el-table-column
+                prop="unitPrice"
+                label="单价(元)"
+                width="200">
+            </el-table-column>
+            <el-table-column
+                prop="totalPrice"
+                label="总价(元)"
+                width="200">
+            </el-table-column>
+            <el-table-column
+                prop="remark"
+                label="备注">
+            </el-table-column>
+          </el-table>
+        </div>
+      </div>
+    </el-card>
+  </div>
 </template>
 
 <script>
+import {deleteTotalPricePromotion} from "@/network/promotion";
+import {formatDate} from "@/common/utils";
+
 export default {
-  name: "TotalPricePromotionList"
+  name: "TotalPricePromotionList",
+  props: {
+    list: Array
+  },
+  data() {
+    return {
+      showAll: []
+    }
+  },
+  mounted() {
+    this.showAll = new Array(this.list.length).fill(false)
+  },
+  methods: {
+    changeState(index) {
+      this.$set(this.showAll, index, !this.showAll[index])
+    },
+    deletePromotion(id) {
+      const config = {
+        params: {
+          id : id
+        }
+      }
+      console.log(config)
+      deleteTotalPricePromotion(config).then(_res => {
+        if (_res.msg === 'Success') {
+          this.$message.success('删除成功！')
+          this.$emit('refresh')
+        }
+      })
+    },
+    formatDate
+  }
 }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+.card {
+  width: 80%;
+  margin: 0 auto;
+  .button {
+    float: right;
+    padding: 3px 0
+  }
+}
+.el-card {
+  margin-bottom: 20px;
+  background: #EEF7F2;
+}
 </style>
